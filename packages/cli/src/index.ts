f7e0e9ea1843dd968b740a977c25d80cb3541130
Michael Lazarski <michael.lazarski@quantilope.com>
2020-01-02 08:38:59 +0200

    add vscode conifg

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -4,17 +4,16 @@ import colors from 'colors';
 import { newProject } from './new';
 
 colors.enable();
-
 program
     .command('new <projectName>')
     .description('Create a new project.')
     .action((projectName: string) => {
-        newProject({projectName})
+        newProject({ projectName })
     })
 
 program.parse(process.argv);
 
 // Shows help if no arguments are provided
 if (process.argv.length === 2) {
-    program.outputHelp();
+    program.outputHelp()
 }
