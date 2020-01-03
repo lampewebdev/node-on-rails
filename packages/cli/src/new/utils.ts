019aa85d46104827a9dc3664026a6ada3c79e48e
Michael Lazarski <michael.lazarski@quantilope.com>
2020-01-03 08:23:43 +0200

    print messages when creating a new project

---
+++ packages/cli/src/new/utils.ts
@@ -0,0 +1,11 @@
+const checkDevDependencies = (): boolean => {
+    return true
+}
+const generateNewProject = ({ projectName }: { projectName: string }): boolean => {
+    return true;
+}
+const installNPMPackages = (): boolean => {
+    return true;
+}
+
+export { checkDevDependencies, generateNewProject, installNPMPackages }
